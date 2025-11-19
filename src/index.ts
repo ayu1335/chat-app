e8ba7f3ce81ec568d073baed5c5df4146c38d3ff
Ayush Thakur <ayushh1335@proton.me>
2025-11-19 23:26:01 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-import WebSocket from "ws";
+import WebSocket, { WebSocketServer } from 'ws';
+const wss = new WebSocketServer({ port: 8080 });
 
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+    ws.on('message', function message(data) {
+        console.log(data.toString());
+        wss.clients.forEach(client => {
+      if (client.readyState === 1) {   // OPEN
+        client.send(`Broadcast: ${data.toString()}`);
+      }
+    });
+    });
+}); 
